027de6a5bfe4f8a6648803dbd4dadb1d020d12f9
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-05 13:06:23 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        ListBox1 = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(293, 235)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Add Items"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 25
+        ListBox1.Location = New Point(257, 89)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(180, 129)
+        ListBox1.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ListBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Adding numbers to a list"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim counter As Integer
+        For counter = 1 To 15
+            ListBox1.Items.Add(counter)
+        Next
+    End Sub
+End Class
